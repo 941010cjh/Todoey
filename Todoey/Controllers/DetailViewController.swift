dacc0ed44d01ced9cd6fd283778f2e75aad2727a
Jihwan <941010cjh@gmail.com>
2021-08-09 22:57:10 +0900

    First commit

---
+++ Todoey/Controllers/DetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailViewController.swift
+//  Todoey
+//
+//  Created by 최지환 on 2021/08/09.
+//  Copyright © 2021 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
